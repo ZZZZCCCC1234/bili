3e0c32a97913aadd1d66ea779897e3d4fc5526ce
z <123@qq.com>
2025-05-22 15:12:36 +0800

    1

--- util/CookieManager.py
+++ util/CookieManager.py
@@ -75,10 +75,11 @@ if __name__ == "__main__":
     # 创建 CookieManager 实例，指定数据库路径（可选）
     cookie_manager = CookieManager(config_file_path="cookies_db.json")
 
+    print("获取到的 Cookies：",  cookie_manager.get_cookies_str())
     # 调用私有方法进行登录并保存 Cookie
-    try:
-        cookies = cookie_manager._login_and_save_cookies()
-        print("获取到的 Cookies：", cookies)
-    except Exception as e:
-        print("登录过程中发生错误：", e)
+    # try:
+    #     cookies = cookie_manager._login_and_save_cookies()
+    #     print("获取到的 Cookies：", cookies)
+    # except Exception as e:
+    #     print("登录过程中发生错误：", e)
 
